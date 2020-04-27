f34e453266cf5ae4861b5521d61108997a1f9d03
Tim <tgaskell23@gmail.com>
2020-04-27 13:14:22 +1000

    Lesson 86 - 106 + Comments
    Implementing saving system

--- RPG Core/Assets/Scripts/Saving/SaveableEntity.cs
+++ RPG Core/Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
-using RPG.Core;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.AI;
+
 
 namespace RPG.Saving
 {
@@ -13,11 +12,20 @@ namespace RPG.Saving
         [SerializeField] string uniqueIdentifier = "";
         static Dictionary<string, SaveableEntity> globalLookup = new Dictionary<string, SaveableEntity>();
 
+        /// <summary>
+        /// Returns a unique Identifier for this object
+        /// </summary>
+        /// <returns> Unique string identifier </returns>
         public string GetUniqueIdentifier()
         {
             return uniqueIdentifier;
         }
 
+        /// <summary>
+        /// Retrieves all Components that have savable data that have an ISavable Interface in them. Each of component will return a dictionary 
+        /// of savable data for their component and save it with a key to the script name
+        /// </summary>
+        /// <returns> Dictionary of Dictionaries with a script name key and dictionary of variables that need to be restored or singular object state instead of dictionary</returns>
         public object CaptureState()
         {
             Dictionary<string, object> state = new Dictionary<string, object>();
@@ -28,6 +36,11 @@ namespace RPG.Saving
             return state;
         }
 
+        /// <summary>
+        /// Restores all Components that have savable data that have an ISavable Interface in them. If the current object has an Isavebale component with a key found in the dictionary then it 
+        /// call for that script to restore its state with the appropriate dictionary for that script
+        /// </summary>
+        /// <param name="state">Typically Dictionary of Dictionaries with a script name key and dictionary of variables that need to be restored. Can refer to singular object state </param>
         public void RestoreState(object state)
         {
             Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
@@ -44,34 +57,39 @@ namespace RPG.Saving
 #if UNITY_EDITOR
         private void Update() {
             if (Application.IsPlaying(gameObject)) return;
-            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return; //Only creates identifier if game isn't running and not in a prefab editing scene
 
             SerializedObject serializedObject = new SerializedObject(this);
             SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
             
-            if (string.IsNullOrEmpty(property.stringValue) || !IsUnique(property.stringValue))
+            if (string.IsNullOrEmpty(property.stringValue) || !IsUnique(property.stringValue))  //Checks if has unique identifier or identifier is not unique
             {
-                property.stringValue = System.Guid.NewGuid().ToString();
-                serializedObject.ApplyModifiedProperties();
+                property.stringValue = System.Guid.NewGuid().ToString(); //Generates unique Identifier for Object
+                serializedObject.ApplyModifiedProperties(); //Overrides uniqueIndentifier with new identifier
             }
 
             globalLookup[property.stringValue] = this;
         }
 #endif
 
+        /// <summary>
+        /// Determines if a unique identifier is already being used by an object. 
+        /// </summary>
+        /// <param name="candidate"> String Unique identifier</param>
+        /// <returns> True or False if the identifier is already being used </returns>
         private bool IsUnique(string candidate)
         {
-            if (!globalLookup.ContainsKey(candidate)) return true;
+            if (!globalLookup.ContainsKey(candidate)) return true; //If no object is using identifier
 
-            if (globalLookup[candidate] == this) return true;
+            if (globalLookup[candidate] == this) return true; //If identifier is this entity
 
-            if (globalLookup[candidate] == null)
+            if (globalLookup[candidate] == null) //If dictionary deleted entity previously
             {
                 globalLookup.Remove(candidate);
                 return true;
             }
 
-            if (globalLookup[candidate].GetUniqueIdentifier() != candidate)
+            if (globalLookup[candidate].GetUniqueIdentifier() != candidate) //If this entity has updated its key, update it
             {
                 globalLookup.Remove(candidate);
                 return true;
--- RPG Core/Assets/Scripts/Saving/SavingSystem.cs
+++ RPG Core/Assets/Scripts/Saving/SavingSystem.cs
@@ -11,6 +11,12 @@ namespace RPG.Saving
 {
     public class SavingSystem : MonoBehaviour
     {
+        /// <summary>
+        /// Function responsible for loading the last scene that was saved. Determines if the saved scene build index is the same as the current scene build index. If not it will load the scene that was 
+        /// saved and restore the states in that save file 
+        /// </summary>
+        /// <param name="saveFile"> Name of Save File</param>
+        /// <returns> Null </returns>
         public IEnumerator LoadLastScene(string saveFile)
         {
             Dictionary<string, object> state = LoadFile(saveFile);
@@ -23,6 +29,10 @@ namespace RPG.Saving
             RestoreState(state);
         }
 
+        /// <summary>
+        /// Function used for saving the game
+        /// </summary>
+        /// <param name="saveFile"> String name of save file</param>
         public void Save(string saveFile)
         {
             Dictionary<string, object> state = LoadFile(saveFile);
@@ -30,16 +40,30 @@ namespace RPG.Saving
             SaveFile(saveFile, state);
         }
 
+        /// <summary>
+        /// Function used for loading a previous save
+        /// </summary>
+        /// <param name="saveFile">String name of save file</param>
         public void Load(string saveFile)
         {
             RestoreState(LoadFile(saveFile));
         }
 
+        /// <summary>
+        /// Deletes save file
+        /// </summary>
+        /// <param name="saveFile"> string name of save file </param>
         public void Delete(string saveFile)
         {
             File.Delete(GetPathFromSaveFile(saveFile));
         }
 
+        /// <summary>
+        /// Returns a dictionary of <Sting,Object> by either creating a new one if there is no save file or deserializing an old save file. 
+        /// This dictionary dictates what saveable entities scripts need to be restored. 
+        /// </summary>
+        /// <param name="saveFile"> String name of save file being loaded</param>
+        /// <returns> Dictionary of dictionaries referring to each object and their dictionary of scripts that need to be restored</returns>
         private Dictionary<string, object> LoadFile(string saveFile)
         {
             string path = GetPathFromSaveFile(saveFile);
@@ -54,6 +78,12 @@ namespace RPG.Saving
             }
         }
 
+        /// <summary>
+        /// Saves the data to the selected save file. It rewrites or creates a file with the selected file name and serializes the save data into binary.
+        /// The binary data is written to the save file. The saved data is a dictionary of dictionaries of each object key and dictionary of scripts that contain restorable data
+        /// </summary>
+        /// <param name="saveFile"> String name of file being created </param>
+        /// <param name="state"> Serializable data being saved </param>
         private void SaveFile(string saveFile, object state)
         {
             string path = GetPathFromSaveFile(saveFile);
@@ -65,6 +95,11 @@ namespace RPG.Saving
             }
         }
 
+        /// <summary>
+        /// Captures the states of all savable entities in the scene. These states are saved in the state dictionary where each entity has a unique identifier. 
+        /// This allows the loading to know which object relates to which state. Also logs what the current scene is to the save dictionary.
+        /// </summary>
+        /// <param name="state"> Dictionary of data being saved to a file </param>
         private void CaptureState(Dictionary<string, object> state)
         {
             foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
@@ -75,6 +110,11 @@ namespace RPG.Saving
             state["lastSceneBuildIndex"] = SceneManager.GetActiveScene().buildIndex;
         }
 
+        /// <summary>
+        /// Loops through all objects that have a saveableEntity script attached to them. Determines whether the save file dictionary contains the unique identifier of the object.
+        /// If it does, there is saved data to be restored and calls for it to be restored for the object.
+        /// </summary>
+        /// <param name="state"> Dictionary of saved data </param>
         private void RestoreState(Dictionary<string, object> state)
         {
             foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
@@ -87,6 +127,11 @@ namespace RPG.Saving
             }
         }
 
+        /// <summary>
+        /// Creates a file path for a save file. This file path leads to where unity places data files on an application. E.g. C:/Users/....
+        /// </summary>
+        /// <param name="saveFile"> String name for save file </param>
+        /// <returns> File path for the save file </returns>
         private string GetPathFromSaveFile(string saveFile)
         {
             return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
